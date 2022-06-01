5e4309a41d91b50dee2a1d621dd0cf6854cc5e3d
HyegeunCho <hgcho84.kr@gmail.com>
2022-06-02 07:21:15 +0900

    [Add] File open

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+    println!("Processing target: {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+    
 }
