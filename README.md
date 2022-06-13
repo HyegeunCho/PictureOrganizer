5ed8c58593d3f0fea22c17dd676864f71cb0a2b1
HyegeunCho <hgcho84.kr@gmail.com>
2022-06-14 07:05:50 +0900

    [MOD] Adding Copy file logic

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # PictureOrganizer
+
+**Support table**
+
+| File type | Exif    | IPTC    | XMP     | ICC     | MPF     | Thumbnail |
+| ----------|---------|---------|---------|---------|---------|-----------|
+| JPEG      | **yes** | **yes** | **yes** | **yes** | **yes** | **yes**   |
+| TIFF      | **yes** | **yes** | **yes** | **yes** | ???     | no        |
+| PNG       | no      | no      | **yes** | no      | no      | no        |
+| HEIC/HEIF | **yes** | no      | no      | **yes** | ???     | no        |
+| WebP      | **yes** | no      | **yes** | **yes** | ???     | **yes**   |
+
+[ ] Exif
+
+[ ] XMP
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,63 @@
 use std::env;
 use std::fs;
 use std::io;
+use std::path::Path;
 use std::fs::File;
 use std::io::Read;
 use futures::executor::block_on;
 
-async fn read_file_as_byte(filename: &String, filesize: usize) -> io::Result<Vec<u8>>{
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let path = Path::new(&args[1]);
+    let path_dir = path.parent();
+    let path_name = path.file_stem();
+    let path_ext = path.extension();
+
+    println!("parent: {:?}", path_dir);
+    println!("name: {:?}", path_name);
+    println!("ext: {:?}", path_ext);
+    println!("path: {:?}", path.to_str());
+
+    let filename = String::from(path.to_str().unwrap());
+    println!("Processing target: {}", filename);
+    
+    let metadata = fs::metadata(&filename).expect("metadata not found");
+    let file_length: usize = metadata.len() as usize;
+    println!("File Length: {}", file_length);
+
+    let future_read_image = read_file_as_byte(&filename, file_length);
+    match block_on(future_read_image) {
+        Ok(buffer) => {
+            println!("Success: {}", buffer.len());
+            let copy_name = format!("{}/{}_{}.{}", path_dir, path_name, "new", path_ext);
+            let future_copy_image = save_file(&buffer, copy_name);
+            match block_on(future_copy_image) {
+                Ok(result) => {
+
+                },
+                Err(e_copy) => {
+                    println!("save failed: {}", e);
+                }
+            }
+
+        },
+        Err(e) => {
+            println!("read failed: {}", e);
+        
+    }
+}
+
+async fn read_file_as_byte(filename: &String, filesize: usize) -> io::Result<Vec<usize>>{
     
     let mut f = File::open(filename).expect("file not found");
-    let mut buffer = vec![0; filesize];
+    let mut buffer = Vec::new<usize>();
 
     f.read_to_end(&mut buffer).expect("error while reading file");
     
     Ok(buffer)
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+async fn save_file(in_buffer: &Vec<usize>, in_filename: &String) -> io::Result<bool>) {
 
-    let filename = &args[1];
-    println!("Processing target: {}", filename);
-
-    
-    let metadata = fs::metadata(filename).expect("metadata not found");
-    let file_length: usize = metadata.len() as usize;
-    println!("File Length: {}", file_length);
-
-    let future = read_file_as_byte(&filename, file_length);
-    match block_on(future) {
-        Ok(buffer) => {
-            println!("Success: {}", buffer.len());            
-        },
-        Err(e) => {
-            println!("read failed: {}", e);
-        }
-    }
 }
-
