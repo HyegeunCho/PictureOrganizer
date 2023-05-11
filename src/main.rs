8d086c57193e0a93905cc42aa6a9c04170630f74
HyegeunCho <hgcho84.kr@gmail.com>
2023-05-11 15:01:43 +0900

    [Update] 디렉토리 입력 받아 파일 순회하는 기능 추가

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,75 @@ use chrono::DateTime;
 
 use exif;
 
-
-
+use walkdir::WalkDir;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
 
+    
     let target_path = &args[1];
     println!("Processing target: {}", target_path);
+    
+    let path = Path::new(target_path);
+    if path.is_file() {
+
+        process_image_file(&target_path);
+    }
+    else if path.is_dir() {
+        // walking
+        let walk_dir = WalkDir::new(target_path);
+
+        for entry in walk_dir {
+            let entry = entry.unwrap();
+            let child_path = entry.path().display().to_string();
+            if entry.metadata().unwrap().is_file() {
+                println!("child: {}", child_path);    
+                process_image_file(&child_path);
+            }
+        }
+
+
+    }
+
+
+}
+
+fn get_created_datetime(in_path: &String) -> DateTime<Utc> {
+
+    let metadata = fs::metadata(in_path).expect("metadata not found");
+    let systemtime = metadata.created().unwrap();
+    let datetime: DateTime<Utc> = systemtime.into();
+
+    datetime
+}
+
+
+
+fn is_exist_file(in_path: &String) -> bool {
+
+    Path::new(&in_path).is_file()
+}
+
+fn is_exist_dir(in_path: &String) -> bool {
+    
+    Path::new(&in_path).is_dir()
+}
+
+fn process_image_file(target_path: &String) {
+
+    let img = image::open(target_path);
+
+    match &img {
+        Ok(img_val) => {
+            let (w, h) = img_val.dimensions();
+            println!("dimesion: ({}, {})", w, h);   
+        }, 
+        Err(error) => {
+            println!("Cannot load image: {}", error);
+            return
+        }
+    }
 
     let datetime = get_created_datetime(&target_path);
     println!("created: {}", datetime.format("%Y/%m/%d %T"));
@@ -26,7 +86,6 @@ fn main() {
     let file_extension_val = path.extension().unwrap().to_string_lossy();
 
     let new_parent_path = format!("classified/{}", datetime.format("%Y_%m_%d"));
-    let is_newdir_exist = Path::new(&new_parent_path).is_dir();
     if !is_exist_dir(&new_parent_path) {
         fs::create_dir_all(&new_parent_path).expect("Cannot create directory");
     }
@@ -49,11 +108,6 @@ fn main() {
         println!("NOT EXIST {}", new_path);
     }
 
-    let img = image::open(target_path).unwrap();
-    let (w, h) = img.dimensions();
-    println!("dimesion: ({}, {})", w, h);   
-    
-
     // read exif data
     let file = std::fs::File::open(path).expect("Cannot open file.");
     let mut bufreader = std::io::BufReader::new(&file);
@@ -74,26 +128,5 @@ fn main() {
     
 
     // TEST
-    img.save(new_path).expect("Cannot save copy image.");
-}
-
-fn get_created_datetime(in_path: &String) -> DateTime<Utc> {
-
-    let metadata = fs::metadata(in_path).expect("metadata not found");
-    let systemtime = metadata.created().unwrap();
-    let datetime: DateTime<Utc> = systemtime.into();
-
-    datetime
-}
-
-
-
-fn is_exist_file(in_path: &String) -> bool {
-
-    Path::new(&in_path).is_file()
-}
-
-fn is_exist_dir(in_path: &String) -> bool {
-    
-    Path::new(&in_path).is_dir()
+    img.unwrap().save(new_path).expect("Cannot save copy image.");
 }
